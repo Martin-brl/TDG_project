74f30d1a3a89e68cae9f41c32f2b45c717b8e410
Jamane92 <nicolas.chaudemanche1@gmail.com>
2023-11-29 00:00:51 +0100

    Ajout de structures

--- header.h
+++ header.h
@@ -12,4 +12,20 @@ struct Operation
     struct Arc* arc;
 };
 
+struct Arc
+{
+    int id_suivant; // numéro de sommet d'un arc adjacent au sommet initial
+    struct Arc* arc_suivant;
+};
+
+/* Alias de pointeur sur un Arc */
+typedef struct Arc* pArc;
+
+struct Operation
+{
+    int id;
+    float temps_dexecution;
+    struct Arc* arc;
+};
+
 #endif //TDG_PROJECT_HEADER_H
